4473d54ef4e30bc97c7dc6892e2f3a876885c37d
Bradley Broom <bmbroom@mdanderson.org>
2025-04-29 05:11:11 +0000

    Testing various color themes

--- .config/nvim/lua/plugins/kanagawa.lua
+++ .config/nvim/lua/plugins/kanagawa.lua
@@ -2,7 +2,17 @@ return {
   "rebelot/kanagawa.nvim",
   config = function()
     require('kanagawa').setup({
-    });
-    vim.cmd("colorscheme kanagawa");
+      overrides = function(colors)
+        return {
+            -- Assign a static color to strings
+            Comment = { fg = colors.palette.carpYellow, italic = false },
+            String = { fg = colors.palette.springGreen, italic = false },
+            Keyword = { fg=colors.palette.dragonGViolet, italic = false },
+            -- theme colors will update dynamically when you change theme!
+            -- SomePluginHl = { fg = colors.theme.syn.type, bold = true },
+        }
+      end
+    })
+    vim.cmd("colorscheme kanagawa-dragon");
   end
 }
