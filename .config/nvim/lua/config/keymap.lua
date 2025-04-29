8c78b6d3ff257c536e6712b675f039753f78848f
Bradley Broom <bmbroom@mdanderson.org>
2025-04-29 03:14:20 +0000

    Change Oil key to <leader>-

--- .config/nvim/lua/config/keymap.lua
+++ .config/nvim/lua/config/keymap.lua
@@ -1,1 +1,1 @@
-vim.keymap.set("n", "-", "<cmd>Oil --float<CR>", { desc="Open parent directory in Oil" })
+vim.keymap.set("n", "<leader>-", "<cmd>Oil --float<CR>", { desc="Open parent directory in Oil" })
